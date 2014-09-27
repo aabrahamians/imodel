ae853216af107cc8acfcf9075876a097babbd851
aabrahamians <aabrahamians@gmail.com>
2014-09-27 15:51:11 -0700

    added paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :picture,
+    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    attachment_size: { less_than: 5.megabytes }
+
+
+has_attached_file :picture,
+:styles => {
+      :original => ['1920x1680>', :jpg, :convert_options => "-auto-orient"],
+      :small    => ['100x100#',   :jpg, :convert_options => "-auto-orient"],
+      :medium   => ['250x250',    :jpg, :convert_options => "-auto-orient"],
+      :large    => ['600x600>',   :jpg, :convert_options => "-auto-orient"]
+    }
+
+
+
 end
