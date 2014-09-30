77787ea5fa82065b188d61702bc4123cb7e16754
aabrahamians <aabrahamians@gmail.com>
2014-09-29 17:46:01 -0700

    gamma gallery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,31 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+	
+			$(function() {
+
+				var GammaSettings = {
+						// order is important!
+						viewport : [ {
+							width : 1200,
+							columns : 5
+						}, {
+							width : 900,
+							columns : 4
+						}, {
+							width : 500,
+							columns : 3
+						}, { 
+							width : 320,
+							columns : 1
+						}, { 
+							width : 0,
+							columns : 2
+						} ]
+				};
+
+				Gamma.init( GammaSettings);
+
+
+			});
